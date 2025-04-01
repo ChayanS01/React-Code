f5db00cbd37f4339e88003bc39bd362a89ae7d85
Chayan Kumar Sen <cks288899@gmail.com>
2025-04-01 21:50:00 +0530

    Context-API react code

---
+++ react-context/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-black text-white p-4 text-3xl'>Hello Context API</h1>
+    </>
+  )
+}
+
+export default App
